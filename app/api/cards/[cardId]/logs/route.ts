a10bbfe5038e7d4d82b766976fd49d161f9f9ed5
Kuba <jakubsliz03@gmail.com>
2024-02-16 16:49:52 +0100

    fix: GET 500 (Internal Server Error)

--- app/api/cards/[cardId]/logs/route.ts
+++ app/api/cards/[cardId]/logs/route.ts
@@ -1,9 +1,10 @@
 import { auth } from '@clerk/nextjs';
+import { ENTITY_TYPE } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
 import { db } from '@/lib/db';
 
-export async function GET(req: Request, { params }: { params: { cardId: string } }) {
+export async function GET(request: Request, { params }: { params: { cardId: string } }) {
   try {
     const { userId, orgId } = auth();
 
@@ -11,25 +12,19 @@ export async function GET(req: Request, { params }: { params: { cardId: string }
       return new NextResponse('Unauthorized', { status: 401 });
     }
 
-    const card = await db.card.findUnique({
+    const auditLogs = await db.auditLog.findMany({
       where: {
-        id: params.cardId,
-        list: {
-          board: {
-            orgId
-          }
-        }
+        orgId,
+        entityId: params.cardId,
+        entityType: ENTITY_TYPE.CARD
       },
-      include: {
-        list: {
-          select: {
-            title: true
-          }
-        }
-      }
+      orderBy: {
+        createdAt: 'desc'
+      },
+      take: 3
     });
 
-    return NextResponse.json(card);
+    return NextResponse.json(auditLogs);
   } catch (error) {
     return new NextResponse('Internal Error', { status: 500 });
   }
--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -1,6 +1,7 @@
-import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
+
+import { db } from '@/lib/db';
 
 export async function GET(req: Request, { params }: { params: { cardId: string } }) {
   try {
